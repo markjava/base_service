ef8c9197341e3402d32be83368006b4ca256f98d
mark_java_xy <mark_java_xy@126.com>
2018-07-11 11:29:53 +0800

    redis

---
+++ base-service-catch/src/main/java/com/base/service/CatchApplication.java
@@ -0,0 +1,15 @@
+package com.base.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * catch application
+ */
+@SpringBootApplication
+public class CatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchApplication.class, args);
+    }
+}
